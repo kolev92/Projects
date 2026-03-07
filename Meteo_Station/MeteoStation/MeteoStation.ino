4fd21515ddf710fe4b36268ebcaf6acbf011041b
kolev92 <125755028+kolev92@users.noreply.github.com>
2026-03-07 11:18:06 +0200

    Add files via upload

---
+++ Meteo_Station/MeteoStation/MeteoStation.ino
@@ -0,0 +1,92 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+#include <LiquidCrystal.h>
+
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+Adafruit_BMP280 bmp; // I2C
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+byte celsius[] = {
+  B11100,
+  B10100,
+  B11100,
+  B00110,
+  B01001,
+  B01000,
+  B01001,
+  B00110
+};
+void setup() {
+  lcd.begin(16,2);
+  Serial.begin(9600);
+  while ( !Serial ) delay(100);   // wait for native usb
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  //status = bmp.begin();
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+   
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+    lcd.createChar(0,celsius);
+    lcd.setCursor(2,0);
+    lcd.print("Meteo Station");
+    delay(4000);
+    lcd.clear();
+}
+
+void loop() {
+    int button=analogRead(A0);
+    Serial.println(button);
+    
+    if(button==0){
+    lcd.clear();
+    lcd.print("Altitude:");
+    lcd.print(bmp.readAltitude(1013.25));
+    delay(3000);
+    lcd.clear();
+    }
+    else{
+    lcd.setCursor(0,0);
+    lcd.print("Temp:");
+    lcd.print(bmp.readTemperature());
+    lcd.write((byte)0);
+
+    lcd.setCursor(0,1);
+    lcd.print("Press:");
+    lcd.print(bmp.readPressure());
+    lcd.print("Pa");
+    
+    Serial.print("Temp:");
+    Serial.print(bmp.readTemperature());
+    Serial.println("C");
+    
+    Serial.print("Press:");
+    Serial.print(bmp.readPressure());
+    Serial.println("Pa");
+
+    Serial.print("Approx altitude = ");
+    Serial.print(bmp.readAltitude(1013.25));
+    Serial.println("m");
+    delay(30);
+    }
+}
